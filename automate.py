001a63a31c3ec57d1ea1d266814b7c13e7834429
Aviv Kerbel <aviv@flytrex.com>
2020-04-19 23:49:20 +0300

    support postgres

--- automate.py
+++ automate.py
@@ -1,67 +1,68 @@
+from multiprocessing import Process
+from threading import Thread
+
 import twint
 import schedule
 import time
 
+hashtags = ['#COVID19',
+            '#C19',
+            'Corona',
+            'CORONA',
+            'corona',
+            '#Corona',
+            '#CORONA',
+            '#corona',
+            'Coronavirus',
+            'CoronaVirus',
+            'CORONAVIRUS',
+            '#Coronavirus',
+            '#CoronaVirus',
+            '#CORONAVIRUS'
+            ]
 
-# you can change the name of each "job" after "def" if you'd like.
 
-
-def job_1():
-    print("Fetching Tweets")
+def job(hashtag):
     c = twint.Config()
-    c.Search = "#COVID19"
+    c.Search = hashtag
     c.Since = "2019-12-01"
-    c.Until = "2020-04-01"
+    c.Until = "2020-05-01"
     c.Lang = "en"
     c.Near = "US"
-    c.Database = 'tweets.db'
-    twint.run.Search(c)
-
-def job_2():
-    print("Fetching Tweets")
-    c = twint.Config()
-    c.Search = "#C19"
-    c.Since = "2019-12-01"
-    c.Until = "2020-04-01"
-    c.Lang = "en"
-    c.Near = "US"
-    c.Database = 'tweets.db'
-    twint.run.Search(c)
-
-def job_3():
-    print("Fetching Tweets")
-    c = twint.Config()
-    c.Search = "#CoronaVirus"
-    c.Since = "2019-12-01"
-    c.Until = "2020-04-01"
-    c.Lang = "en"
-    c.Near = "US"
-    c.Database = 'tweets.db'
-    twint.run.Search(c)
-
-def job_4():
-    print("Fetching Tweets")
-    c = twint.Config()
-    c.Search = "#StayHome"
-    c.Since = "2019-12-01"
-    c.Until = "2020-04-01"
-    c.Lang = "en"
-    c.Near = "US"
-    c.Database = 'tweets.db'
+    c.Database = 'new_tweets.db'
     twint.run.Search(c)
 
 
-# run once when you start the program
-job_1()
-job_2()
-job_3()
-job_4()
+def fetch_hashtags():
+    for hashtag in hashtags:
+        job(hashtag)
 
-schedule.every().second.do(job_1)
-schedule.every().second.do(job_2)
-schedule.every().second.do(job_3)
-schedule.every().second.do(job_4)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+# fetch_hashtags()
+
+# while True:
+#     thread = Thread(target=fetch_hashtags)
+#     thread.start()
+#     time.sleep(1)
+
+def job():
+    c = twint.Config()
+    c.Search = '#COVID19'
+    c.Since = "2019-12-01"
+    c.Until = "2020-04-15"
+    c.Lang = "en"
+    c.Near = "US"
+    c.Database = 'tweets'
+    twint.run.Search(c)
+
+
+job()
+
+# schedule.every().second.do(job)
+
+# while True:
+#     # schedule.run_pending()
+#     thread = Thread(target=job)
+#     thread.start()
+#     time.sleep(0.05)
+
